a59e602772b37148f962f3c7b275fb89a22081e4
Dom Mentee <chuku0929@DomsMacBP.local>
2019-04-05 12:33:06 -0400

    added first component

---
+++ client/components/UsernameForm.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+class UsernameForm extends React.Component<{}, {}> {
+
+}
+
+
+export default UsernameForm;
--- client/index.tsx
+++ client/index.tsx
@@ -1,13 +1,18 @@
 // React
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+
+//imported files
 import './index.scss';
+import Cookie from "js-cookie";
+import UsernameForm from "./components/UsernameForm";
+
 
 // Import App, which is the main react component
 class App extends React.Component {
   render() {
     return (
-      <div className="appbackground">
+      <div className="main">
         This is basic react!
       </div>
     );
