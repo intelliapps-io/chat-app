9e1af425f53ecd7da01dbbcb2b5b2df90f0c34f2
Dom Mentee <chuku0929@DomsMacBP.fios-router.home>
2019-04-12 19:53:17 -0400

    added message.tsx component

---
+++ client/components/Message.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+
+interface IState {
+
+}
+
+class Message extends React.Component<{}, IState> {
+  render() {
+    return (
+      <div className="message">
+      </div>
+    )
+  }
+
+}
+export default Message;
