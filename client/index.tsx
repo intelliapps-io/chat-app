c61784e71b49e15e1a53c85123375c61c907a83a
Intelliapps <41976910+intelliapps-io@users.noreply.github.com>
2019-04-27 21:56:24 -0400

    Changed state to update new message on send

--- client/index.tsx
+++ client/index.tsx
@@ -6,91 +6,65 @@ import * as ReactDOM from 'react-dom';
 import './index.scss';
 import Chatkit from '@pusher/chatkit-client';
 import RoomList from './components/RoomList';
-import MessageList, {IMassage} from './components/MessageList';
+import MessageList, { IMessage } from './components/MessageList';
 import SendMessageForm from './components/SendMessageForm';
 import NewRoomForm from './components/NewRoomForm';
 import { tokenUrl, instanceLocator } from './chatkit'
 
 interface IState {
-  roomId: number
+  roomId: string
   currentUser: any //set state of curent user
-  messages: IMassage[] //state of messages
+  messages: IMessage[] //state of messages
 }
-//State is private to a component
-//props is is not provate and is shared between compoents
-// Import App, which is the main react component
+
 class App extends React.Component<{}, IState> {
   currentUser: any
   constructor(props) {
     super(props)
     this.state = {
-      roomId: 20580718,
+      roomId: "20580718",
       currentUser: null,
-      messages: []// set to an empty array
+      messages: [] // set to an empty array
     }
-    
   }
 
-
   componentDidMount() {
-    const chatManager = new Chatkit.ChatManager ({
+    const chatManager = new Chatkit.ChatManager({
       instanceLocator,
       userId: 'slick-Rick',
-      tokenProvider: new Chatkit.TokenProvider({
-        url: tokenUrl
-      })
-    })//after connection has been estiblished with api
+      tokenProvider: new Chatkit.TokenProvider({ url: tokenUrl })
+    }) //after connection has been estiblished with api
 
     chatManager.connect().then(currentUser => {
       this.setState({ currentUser });
       currentUser.subscribeToRoom({
-        roomId: this.state.roomId.toString(),
+        roomId: this.state.roomId,
         hooks: {
-          onMessage: (message: IMassage) => {
+          onMessage: (message: IMessage) => {
             let messages = this.state.messages
-            messages.push(message)
-            //this.setState({messages})
-            console.log(message)
+            messages.push(message);
+            this.setState({ messages });
           }
         }
-      });
-
-      currentUser.fetchMultipartMessages({
-        roomId: this.state.roomId.toString(),
       })
-        .then(messages => {
-          console.log(messages)
-          this.setState({ messages });
-          // do something with the messages
-
-        })
-        .catch(err => {
-          console.log(`Error fetching messages: ${err}`)
-        })
-
     })
-
   }
 
   sendMessage(text: string) {
-    this.state.currentUser.sendMessage({
-      text,
-      roomId: this.state.roomId.toString()
-    })
-
-    //need to use inverse data flow
+    const { roomId } = this.state
+    this.state.currentUser.sendMessage({ text, roomId })
   }
 
   render() {
-    const { currentUser } = this.state;
+    const { currentUser, messages } = this.state;
     const isLoading = currentUser === null;
     if (isLoading) return (<h1>Loading...</h1>);
     return (
       <div className="main">
         <RoomList />
-        <MessageList messages={this.state.messages}/> {/*how you pass down props to component*/}
-        <SendMessageForm sendMessage={this.sendMessage.bind(this)}/>
-        <NewRoomForm/>
+        <MessageList messages={messages} /> {/*how you pass down props to component*/}
+        <SendMessageForm sendMessage={this.sendMessage.bind(this)} />
+        <NewRoomForm />
       </div>
     );
   }
