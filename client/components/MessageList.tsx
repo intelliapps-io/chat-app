2dc18e516d354fd89222797ab5a2bbb4cd12659e
Intelliapps <41976910+intelliapps-io@users.noreply.github.com>
2019-04-27 21:55:19 -0400

    Changed IMessage interface structure

--- client/components/MessageList.tsx
+++ client/components/MessageList.tsx
@@ -1,32 +1,26 @@
 import * as React from 'react';
 import Message from './Message';
 
-
-export interface IMassage { //exported the massages state 
-  senderId: string //senderId in api
-  username: any
-  parts: [{//to obtain messages from api, how it is nested 
-    payload: {// what was send
-      content: string//actual message
-    }
-  }]
+export interface IMessage {
+  id: number
+  senderId: string
+  text: string
+  roomId: string
+  createdAt: string
 }
 
 interface IProps { //messages property
-  messages: IMassage[] //will contain senderId and content
-
+  messages: IMessage[] //will contain senderId and content
 }
 
 class MessageList extends React.Component<IProps> {
-
   render() {
+    const { messages } = this.props;
     return (
       <div className="message-list">
-        {this.props.messages.map((message, index) => {//map the properties of the fetch data
-          return (
-            <Message key={index} username={message.senderId} content={message.parts[0].payload.content}/> /*how you pass down props to child component*/
-          );
-        })}
+        {messages.map(({ id, senderId, text }) =>
+          <Message key={id} username={senderId} content={text} />
+        )}
       </div>
     )
   }
